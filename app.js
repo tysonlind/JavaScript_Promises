3d89b7fabb0a34d6142d979782ad975cee938a98
tyson lind <tysonfloydlind@gmail.com>
2022-11-14 09:06:25 -0700

    completed assignemnt

--- app.js
+++ app.js
@@ -15,10 +15,48 @@ function getList() {
   });
 }
 
-// TODO: Handle the resolved or rejected states of the promise
+// selecting the error paragraph
+let errDiv = document.querySelector("#error");
+let ulEle = document.querySelector("#list");
+let promise=getList();
+
+
+promise.then(res =>
+  res.forEach(function (hobbit){
+    let li = document.createElement("li");
+    li.textContent = hobbit;
+    ulEle.appendChild(li);
+    })
+  ).catch((err) => {
+    console.error(err);
+    errDiv.textContent = err.message;
+  })
+
+//written as an async/await
+/*
+async function updateDOM() {
+  try {
+    let list = await getList();
+    list.forEach((hobbit) => {
+      let li = document.createElement("li");
+      li.textContent = hobbit;
+      ulEle.appendChild(li);
+    });
+  } catch (err) {
+    console.error(err);
+    err.textContent = err.message;
+  }
+}
+updateDOM(); */
 
 // TODO: If the promise resolves with the list of hobbits
-// Render the list of hobbits as list items within the unordered list with id="list" (check the index.html file)
+// Render the list of hobbits as list items within the unordered list with id="list" 
+// TODO: If promise rejects
+// Display error message in the paragraph element with id="error" 
 
-// TODO: If the promise rejects with the failure object
-// Display the failure message in the paragraph element with id="error" (check index.html file)
+
+/*Replace the console.log statement in the then method callback parameter to
+iterate through the resolved list of hobbits
+create a li for each hobbit, and append the li to the selected ul from the DOM
+Replace the console.log statement in the catch method callback parameter to
+display the resolved failure object's message property as the text content of the selected p from the DOM*/
